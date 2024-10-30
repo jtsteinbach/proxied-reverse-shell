06b7e376eb82540ce554553f1a545e0e953ca86d
JT <166568397+jt-steinbach@users.noreply.github.com>
2024-10-30 02:23:26 -0700

    Update netninja_server.py

--- netninja_server.py
+++ netninja_server.py
@@ -19,7 +19,7 @@ redis_host = f"{parsed_url.hostname}:{parsed_url.port}"
 
 # Initialize Redis connection
 redis_client = redis.Redis(
-    host=f'{redis_host}',
+    host=f'{parsed_url.hostname}',
     port=int(parsed_url.port),
     password=f'{redis_pass}',
     db=0,

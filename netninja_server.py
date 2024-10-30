42e40514b654b455ff01f81d98541f6b3eb69b76
JT <166568397+jt-steinbach@users.noreply.github.com>
2024-10-30 02:07:17 -0700

    Update netninja_server.py

--- netninja_server.py
+++ netninja_server.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 redis_client = redis.Redis(
     host='net-ninja-main-4842.redis.b.osc-fr1.scalingo-dbs.com',
     port=30757,
-    password='YjCU72yX28B@@Pz',
+    password='wJz_jmMYj7Xq7zUyKeqy8OqZ_Z0AIkC8798xf5_xoUjNCOWu_VXNR_kmCxXJ7Kh0',
     db=0,
     decode_responses=True
 )
